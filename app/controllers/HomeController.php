20bff7ba1da6182a59b00064d9a5d6e71d3f08f9
yerman21 <danyaguirre@upeu.edu.pe>
2017-07-05 21:55:43 -0500

    ya hace peticiones JSON

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,25 +8,19 @@ class HomeController extends \Phalcon\Mvc\Controller
 		$this->persistent->parameters = null;
 		header("Access-Control-Allow-Origin:*");	
 		//url contra la que atacamos
-		$ch = curl_init("http://localhost:8090/Cliente/datoss.php");
-
-		//a true, obtendremos una respuesta de la url, en otro caso, 
-		//true si es correcto, false si no lo es
-		//establecemos el verbo http que queremos utilizar para la petición
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));          
-
-		//enviamos el array data
-		//curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-		//obtenemos la respuesta
+		$ch = curl_init("http://localhost:8000/myhous/");		
+		curl_setopt($ch, CURLOPT_HTTPGET, true);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));          
 		$response = curl_exec($ch);
 		// Se cierra el recurso CURL y se liberan los recursos del sistema
 		curl_close($ch);
-		$config =new Phalcon\Config\Adapter\Json("http://localhost:8090/Cliente/datoss.php");
+		//Para solicitar JSON... pero con un formato especial ... los clave:"/value/"
+		//$config =new Phalcon\Config\Adapter\Json("http://localhost:8090/Cliente/datoss.php");
 
-		$var =$config->city;
+		//$var =$config->city;
 		
-		$this->view->setVar('datos',$config);
+		$this->view->setVar('datos',$response);
 	}
 
 }
--- app/views/home/busca.volt
+++ app/views/home/busca.volt
@@ -8,7 +8,8 @@
 </div>
 
 <script type="text/javascript">
-   console.log({{ datos.name }});
+var json={{ datos }};
+   console.log(json);
 </script>
 
 
