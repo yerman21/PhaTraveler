4afd8068d7b5f554be23d863b1a7bba85ccbc18c
yerman21 <danyaguirre@upeu.edu.pe>
2017-07-06 22:35:26 -0500

    vistas echas para los servicios

---
+++ app/controllers/RestauranteController.php
@@ -0,0 +1,12 @@
+<?php
+
+class RestauranteController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
---
+++ cache/c__xampp_htdocs_phatraveler_app_views_home_restaurante.volt.php
@@ -0,0 +1,29 @@
+<div class="page-header">
+    <h1>
+        Pagina de Restaurante
+    </h1>
+    <p>
+        <?= $this->tag->linkTo(['home/', 'Create carga']) ?>
+    </p>
+</div>
+
+<script type="text/javascript">
+var json=<?= $datos ?>;
+   console.log(json);
+</script>
+
+
+<div class="form-group">
+    <label for="fieldIdalumno" class="col-sm-2 control-label">Idalumno</label>
+    <div class="col-sm-10">
+      
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+        <?= $this->tag->submitButton(['Search', 'class' => 'btn btn-default']) ?>
+    </div>
+</div>
+
+
