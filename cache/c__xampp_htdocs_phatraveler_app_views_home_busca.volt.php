f8d2a855b31caf89031e1cfbeb072ebb28e0f511
yerman21 <danyaguirre@upeu.edu.pe>
2017-07-07 15:45:57 -0500

    vista mejor

--- cache/c__xampp_htdocs_phatraveler_app_views_home_busca.volt.php
+++ cache/c__xampp_htdocs_phatraveler_app_views_home_busca.volt.php
@@ -1,40 +1,15 @@
+<head>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+</head>
 <div class="page-header">
     <h1>
         Pagina de Alquiler
     </h1>
 </div>
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript">
-var json=<?= $datos ?>;
-var jsonResi=json.Residencia;
-var jsonCosas=json.Cosas;
-   console.log(jsonResi);
-   console.log(jsonCosas);
+<section id="content">
 
-lista(jsonResi);
-
-    function lista(tjson){
-        $.each(tjson,function(){
-            $.each(this,function(name,value){
-            console.log(name+" = "+value);
-              if(value instanceof String){
-                lista(value);
-              }
-            });
-        });
-    }
-</script>
-
-<div>
-    <table>
-        <thead>
-            <th>Restaurante</th>
-        </thead>
-        <tbody  id="content">
-            <tr><td></td></tr>
-        </tbody>
-    </table>
-</div>
+</section>
 
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
@@ -42,4 +17,25 @@ lista(jsonResi);
     </div>
 </div>
 
+<script type="text/javascript">
+$(document).ready(function(){
+    var json=<?= $datos ?>;
+    var jsonResi=json.Residencia;
+    var jsonCosas=json.Cosas;
+    console.log(jsonResi);
+    lista(jsonResi);
+});
+
+
+    function lista(tjson){
+        tds="";
+        $.each(tjson, function(i,values){
+
+        tds="<div class='bloque_hous'><div class='img bloque'></div><div class='bloque_datos'><table><tr><th>Residencia: </th><td>"+tjson[i].casa.nombre+"</td><tr><th>Direccion: </th><td>"+tjson[i].casa.direccion+"</td></tr><tr><th>Telefono: </th><td>"+tjson[i].casa.telefono+"</td></tr><tr><th>Zona: </th><td>"+tjson[i].casa.zona+"</td></tr></table></div></div>";
+         $("#content").append(tds);
+        console.log(tds);       
+        });
+        
+    }
+</script>
 
